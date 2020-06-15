b0b009d34f29d63894134e68a1fad6bb59529ff3
jdivincenzo <jdivincenzo@gmail.com>
2020-06-15 14:31:36 -0300

    Boton demorar turno en la pantallade detalle del turno

--- Aplicacion/UseCases/Cliente/DemorarTurno/DemorarTurnoUC.cs
+++ Aplicacion/UseCases/Cliente/DemorarTurno/DemorarTurnoUC.cs
@@ -1,21 +1,25 @@
 ﻿using Aplicacion.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Aplicacion.UseCases.Cliente
 {
     public class DemorarTurnoUC : IUseCase
     {
-        private readonly IRepository _repository;
+        IRepository _repository;
 
-        public DemorarTurnoUC(IRepository repo) {
+        public DemorarTurnoUC(IRepository repo)
+        {
             _repository = repo;
         }
 
         public DemorarTurnoResponse Procesar(DemorarTurnoRequest req)
         {
-            var turnero = _repository.Turneros.Find(req.IdTurnero);
+            //var turnero = _repository.Turneros.Find(req.IdTurnero);
+            var turnero = _repository.Turneros.Include(t => t._turnos).Single(turnero => turnero.Id == req.IdTurnero);
             turnero.Demorar(req.IdTurno);
             return new DemorarTurnoResponse { };
         }
