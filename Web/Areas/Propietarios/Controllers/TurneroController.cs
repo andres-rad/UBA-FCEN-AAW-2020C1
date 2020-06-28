f3df905696aa4f0985e39dc7bc5d04f856b996e9
rbenitti <raulbenitti@gmail.com>
2020-06-28 19:36:53 -0300

    Saco replace(',', '.') y agrego default localization en-US (problema de las coordenadas)

--- Web/Areas/Propietarios/Controllers/TurneroController.cs
+++ Web/Areas/Propietarios/Controllers/TurneroController.cs
@@ -55,7 +55,7 @@ namespace Web.Areas.Propietarios.Controllers
                 Calle = turnero.Calle,
                 Numero = turnero.Numero,
                 Concepto = turnero.Concepto,
-                Ubicacion = new LatLon(double.Parse(turnero.Latitud.Replace('.',',')), double.Parse(turnero.Longitud.Replace('.', ','))),
+                Ubicacion = new LatLon(double.Parse(turnero.Latitud), double.Parse(turnero.Longitud)),
                 CantidadMaxima = turnero.CantidadMaxima,
                 Files = filePaths
             };
